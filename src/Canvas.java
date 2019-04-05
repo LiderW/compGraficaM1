d93b6e796cdc12ffe9cb706f3937f3dfec46a4fe
Pedro Prado <pedrovprado@hotmail.com>
2019-04-05 11:11:17 -0300

    Fixed Bresenham filling and grid detection

--- src/Canvas.java
+++ src/Canvas.java
@@ -12,6 +12,7 @@ public class Canvas {
     private JPanel panel;
     private ArrayList<JButton> buttons;
     private BufferedImage image;
+    private ArrayList<Rectangle> gridRectangles;
 
     private static final int WIDTH = 1200;
     private static final int HEIGHT = 780;
@@ -42,6 +43,7 @@ public class Canvas {
         frame.setVisible(false);
 
         buttons = new ArrayList<>();
+        gridRectangles = new ArrayList<>();
     }
 
     public void createButton(String buttonName){
@@ -64,6 +66,26 @@ public class Canvas {
     public void drawBresenham(ArrayList<Point2D> bresenhamLine){
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, this);
+            }
+        };
+
+        panel.setBackground(Color.BLACK);
+
+        frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.add(buttonsPanel, BorderLayout.NORTH);
+        frame.setTitle("test");
+        frame.setSize(new Dimension(WIDTH, HEIGHT));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(false);
+
         Graphics g = image.getGraphics();
 
         /*for(int j = 40; j < HEIGHT; j += 40){
@@ -74,15 +96,26 @@ public class Canvas {
             g.drawLine(i, 0, i, HEIGHT);
         }*/
 
+        int rectangleSize = 40;
+
         for(int i = 0; i < WIDTH; i += 40){
             for(int j = 0; j < HEIGHT; j += 40){
-                Rectangle rect = new Rectangle(i, j, 40, 40);
+                gridRectangles.add(new Rectangle(i, j, rectangleSize, rectangleSize));
                 g.drawRect(i, j, 40, 40);
             }
         }
 
+        int fillSize = rectangleSize / 2;
         for (Point2D point:bresenhamLine){
-            image.setRGB((int)point.getX(), (int)point.getY(), Color.YELLOW.getRGB());
+            for (Rectangle r:gridRectangles) {
+                if(r.contains(point.getX(), point.getY()) && r.getX() != point.getX() && r.getY()!= point.getY()) {
+                    g.setColor(Color.gray);
+                    g.fillRect(r.x + fillSize/2, r.y + fillSize/2, fillSize, fillSize);
+                }
+            }
+            //g.setColor(Color.yellow);
+            //g.fillRect((int)point.getX(), (int)point.getY(), 1, 1);
+            image.setRGB((int)point.getX(), (int)point.getY(), Color.yellow.getRGB());
         }
 
         g.drawImage(image, WIDTH, HEIGHT, null);
